a09e1c7de2128dcd7e4d1bda01fa25485e2b35f0
Loic Huder <loic.huder@esrf.fr>
2021-08-02 08:16:03 +0200

    Update to JupyterLab v3.1.x

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     cmdclass=cmdclass,
     packages=setuptools.find_packages(),
-    install_requires=["jupyter_server>=1.6,<2", "h5grove>=0.0.8"],
+    install_requires=["jupyter_server>=1.6,<2", "h5grove>=0.0.11"],
     extras_require={"full": ["hdf5plugin"]},
     python_requires=">=3.6",
     zip_safe=False,
--- src/fileType.ts
+++ src/fileType.ts
@@ -1,3 +1,4 @@
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 import { hdf5Icon } from './icons';
 
 /* The file format is set for the following reasons:
@@ -6,7 +7,7 @@ import { hdf5Icon } from './icons';
   - If set to `base64`, it succeeds but no use of this data is made in the extension.
   - Indeed, the fetching of HDF5 is completely enclosed in the H5web app. The file format should not be relevant.
 As a result, this should be ideally changed for a cleaner solution that avoids fetching unnecessary data. */
-const HDF5_FILE_TYPE = {
+const HDF5_FILE_TYPE: Partial<DocumentRegistry.IFileType> = {
   name: 'hdf5file',
   icon: hdf5Icon,
   displayName: 'HDF5 File',
