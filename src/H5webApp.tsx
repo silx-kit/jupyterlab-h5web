d3172f84ff1cee7fd4c5187f402cdc9cc10a97e1
Loic Huder <loic.huder@esrf.fr>
2021-11-26 10:59:20 +0100

    Update h5web from 1.1.0 to 1.2.0

--- src/H5webApp.tsx
+++ src/H5webApp.tsx
@@ -11,6 +11,7 @@ function H5webApp(props: { filePath: string }) {
       <H5GroveProvider
         url={`${baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl}/h5web`}
         filepath={filePath}
+        axiosParams={{ file: filePath }}
       >
         <App />
       </H5GroveProvider>
